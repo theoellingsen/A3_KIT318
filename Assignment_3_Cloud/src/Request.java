b3c9e36b118ee64367605ae7ca64778e0c401540
theoellingsen <te1@utas.edu.au>
2022-05-27 14:35:50 +1000

    Adding Request.java file made by Josh - To be implemented in CLientHandler

---
+++ Assignment_3_Cloud/src/Request.java
@@ -0,0 +1,15 @@
+/*
+ * @author Joshua Perrin
+ * KIT318
+ * Request class definition
+ * Defines the request object type
+ */
+
+public class Request {
+
+	//request constructor
+    public Request(String requestingUsername, String type, String stringInput, String inputFilename, 
+    		int deadline, int code, String status, String stringOutput, String outputFilename) {
+
+    }
+}
